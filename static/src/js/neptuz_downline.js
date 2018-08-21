82ca44e9ce389e07f8d77b6c7a7475fb9bf03425
sung <khisungz@gmail.com>
2018-08-21 19:48:12 +0700

    Javascript got define POS PaymentScreenWidget

--- static/src/js/neptuz_downline.js
+++ static/src/js/neptuz_downline.js
@@ -1,49 +1,64 @@
 // openerp.neptuz_downline = function(instance){
 //     var module   = instance.point_of_sale;
-//     var round_pr = instance.web.round_precision
+//     var round_pr = instance.web.round_precision;
 //     var QWeb     = instance.web.qweb;
+ 
 
-//     console.log("POS JS Loaded aaaa",module)
+    // console.log("POS JS Loaded aaaa",models)
+
+    // models.TheWidgetThatContainsTheFunction.include({
+    //     the_function_to_modify: function(){        //don't forget to include function vars!
+    //          this.super();
+
+    //          console.log("POS JS Loaded bbbbb",models)
+    //          // ... do stuff here
+    //          // return if necessary
+    //     },
+    // });
     
 
 // };
 
-openerp.neptuz_downline = function (instance) {
-    var _t = instance.web._t;
-    var QWeb = instance.web.qweb;
-    var module = instance.point_of_sale;
+odoo.define('point_of_sale.screens', function (require) {
+    "use strict";
 
-    module.PosDB.include({
-        init: function(options){
-            var self = this;
-            this._super();
-        },
-        search_product_in_category: function(category_id, query){
 
-            //You can add your custom code here
-    
-                try {
-                    query = query.replace(/[\[\]\(\)\+\*\?\.\-\!\&\^\$\|\~\_\{\}\:\,\\\/]/g,'.');
-                    query = query.replace(/ /g,'.+');
-                    var re = RegExp("([0-9]+):.*?"+query,"gi");
-                }catch(e){
-                    return [];
-                }
-                var results = [];
-                for(var i = 0; i < this.limit; i++){
-                    r = re.exec(this.category_search_string[category_id]);
-                    if(r){
-                        var id = Number(r[1]);
-                        results.push(this.get_product_by_id(id));
-                    }else{
-                        break;
-                    }
-                }
-                return results;
-            },
-        });
-    
-        console.log("POS JS Loaded aaaa",module)
-        console.log("POS JS Loaded bbb",this)
-        console.log("POS JS Loaded cccc",results)
-    };
+var PosBaseWidget = require('point_of_sale.BaseWidget');
+var gui = require('point_of_sale.gui');
+var models = require('point_of_sale.models');
+var core = require('web.core');
+var Model = require('web.DataModel');
+var utils = require('web.utils');
+var formats = require('web.formats');
+
+var QWeb = core.qweb;
+var _t = core._t;
+
+var round_pr = utils.round_precision;
+
+
+var ScreenWidget = PosBaseWidget.extend({
+
+    init: function(parent,options){
+        this._super(parent,options);
+        this.hidden = false;
+    },
+    });   
+
+
+var PaymentScreenWidget = ScreenWidget.extend({
+    template:      'PaymentScreenWidget',
+    back_screen:   'product',
+    init: function(parent, options) {
+        var self = this;
+        this._super(parent, options);
+    },
+    });
+
+    gui.define_screen({name:'payment', widget: PaymentScreenWidget});
+
+
+console.log("POS JS Loaded aaaa",PaymentScreenWidget);
+
+
+});
